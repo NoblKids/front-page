cb2648b88007d1e5646d8127501bf01fa9224f09
HuseynAkh <huseyn.akh@gmail.com>
2023-11-12 22:44:53 -0500

    update

--- app/components/Newsletter/Newsletter.tsx
+++ app/components/Newsletter/Newsletter.tsx
@@ -22,7 +22,7 @@ const Newsletter = () => {
                     {/* COLUMN-2 */}
                     <div className="p-10 flex flex-col justify-center">
                         <h3 className="flex justify-center text-4xl md:text-5xl font-semibold mb-3 text-white">Get in touch</h3>
-                        <a className="flex justify-center underline mt-2 font-text-base font-extrabold mb-7 text-offwhite" href="mailto:support@noblkids.com">support@noblkids.com</a>
+                        <a className="flex justify-center underline mt-2 font-text-base text-lg font-bold mb-7 text-slate-800" href="mailto:support@noblkids.com">support@noblkids.com</a>
                         {/* <div className="flex gap-0">
                             <input type="Email address" name="q" className="py-4 text-sm w-full text-black bg-white rounded-l-lg pl-4" placeholder="@enter email-address" autoComplete="off" />
                             <button className="bg-midblue text-white font-medium py-2 px-4 rounded-r-lg">
--- app/components/Provide/index.tsx
+++ app/components/Provide/index.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useInView } from 'react-intersection-observer';
+
+import marketing from '/public/FrontPage/provide/marketing.svg';
+import graphic from '/public/FrontPage/provide/graphic.svg';
+import heaking from '/public/FrontPage/provide/heaking.svg';
+import uidesign from '/public/FrontPage/provide/uidesign.svg';
+
+import arrow from '/public/FrontPage/provide/arrow.svg'
 
 interface datatype {
     imgSrc: string;
@@ -9,25 +18,25 @@ interface datatype {
 
 const Aboutdata: datatype[] = [
     {
-        imgSrc: "/assets/provide/marketing.svg",
+        imgSrc: uidesign,
         country: "Booking",
         paragraph: 'Simplified enrolment for activities',
 
     },
     {
-        imgSrc: "/assets/provide/graphic.svg",
+        imgSrc: graphic,
         country: "Payments",
         paragraph: 'Convenient payment processing',
 
     },
     {
-        imgSrc: "/assets/provide/heaking.svg",
+        imgSrc: heaking,
         country: "Class Management",
         paragraph: 'Track student information and attendance',
 
     },
     {
-        imgSrc: "/assets/provide/uidesign.svg",
+        imgSrc: marketing,
         country: "Customer Experience",
         paragraph: 'Easy customer experience and onboarding',
     },
@@ -35,6 +44,20 @@ const Aboutdata: datatype[] = [
 
 
 const Provide = () => {
+
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+    });
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        if (inView) {
+            setIsVisible(true);
+        }
+    }, [inView]);
+
+
     return (
         <div id="services">
 
@@ -44,22 +67,24 @@ const Provide = () => {
                     {/* COLUMN-1 */}
                     <div className='col-span-6 flex justify-center'>
                         <div className="flex flex-col align-middle justify-center p-10">
-                            <p className="text-4xl lg:text-6xl pt-4 font-semibold lh-81 mt-5 text-center lg:text-start">Free Class Management</p>
-                            <h4 className="text-lg pt-4 font-normal lh-33 text-center lg:text-start text-bluegray">Easily integrate with Nobl Inc. for free class management, payments, and customer outreach</h4>
-                            <Link href={'/'} className="mt-4 text-xl font-medium text-blue flex gap-2 mx-auto lg:mx-0 space-links">Learn more <Image src={'/assets/provide/arrow.svg'} alt={'arrow'} width={20} height={20} /></Link>
+                            <p className="text-4xl text-slate-800 lg:text-6xl pt-4 font-semibold lh-81 mt-5 text-center lg:text-start">Free Class Management</p>
+                            <h4 className="mt-3 text-xl pt-4 font-normal lh-33 text-center lg:text-start text-gray-800">Easily integrate with Nobl Inc. for free class management, payments, and customer outreach</h4>
+
+                            {/* <Link href={'/'} className="mt-4 text-xl font-medium text-blue flex gap-2 mx-auto lg:mx-0 space-links">Learn more <Image src={arrow} alt={'arrow'} width={20} height={20} /></Link> */}
                         </div>
                     </div>
 
                     <div className='lg:col-span-1'></div>
 
                     {/* COLUMN-2 */}
-                    <div className='col-span-6 lg:col-span-5'>
+                    <div className={`col-span-6 lg:col-span-5 ${isVisible ? 'opacity-100' : 'opacity-0'}`} 
+                        style={{ transition: 'opacity 2s' }} ref={ref}>
                         <div className='grid grid-cols-1 sm:grid-cols-2 gap-x-16 gap-y-10 lg:gap-x-40 px-10 py-12 bg-bluebg rounded-3xl'>
                             {Aboutdata.map((item, i) => (
                                 <div key={i} className='bg-white rounded-3xl lg:-ml-32 p-6 shadow-xl'>
                                     <Image src={item.imgSrc} alt={item.imgSrc} width={64} height={64} className="mb-5" />
-                                    <h4 className="text-2xl font-semibold">{item.country}</h4>
-                                    <h4 className='text-lg font-normal text-bluegray my-2'>{item.paragraph}</h4>
+                                    <h4 className="text-2xl font-semibold text-slate-800">{item.country}</h4>
+                                    <h4 className='text-lg font-normal text-gray-700 my-2'>{item.paragraph}</h4>
                                 </div>
                             ))}
                         </div>
